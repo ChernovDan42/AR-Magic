8c0c42040dbc0d3c566d7e7f6fe8cc7e51bf7dbe
Dan Chernov <120645560+ChernovDan42@users.noreply.github.com>
2024-02-28 10:15:56 +0100

    some fixes

---
+++ js/email.js
@@ -0,0 +1,21 @@
+const formRef = document.querySelector('.email-form');
+
+formRef.addEventListener('submit', e => {
+  e.preventDefault();
+  sendEmail(e);
+});
+
+function sendEmail(e) {
+  const email = e.target.elements.email.value;
+  const subject = 'New Email Subscription';
+  const body = 'You have a new email subscription from: ' + email;
+
+  const mailtoLink =
+    'mailto:logi5702733@gmail.com' +
+    '?subject=' +
+    encodeURIComponent(subject) +
+    '&body=' +
+    encodeURIComponent(body);
+
+  window.location.href = mailtoLink;
+}
